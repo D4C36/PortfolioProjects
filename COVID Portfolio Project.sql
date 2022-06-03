461e5379737b27852ae41b51be7777fcbe2fea61
Derrick Chiou <dchiou@usc.edu>
2022-06-03 12:52:05 -0700

    Update COVID Portfolio Project.sql

--- COVID Portfolio Project.sql
+++ COVID Portfolio Project.sql
@@ -1,3 +1,8 @@
+/*
+Covid 19 Data Exploration 
+Skills used: Joins, CTE's, Temp Tables, Windows Functions, Aggregate Functions, Creating Views, Converting Data Types
+*/
+
 Select *
 FROM PortfolioProject..CovidDeaths$
 WHERE continent is not NULL
